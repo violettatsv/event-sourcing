69cecd884081c8527a0f13fc6214d7114025b926
Виолетта Цветкова <violettacvetkova@Violettas-MacBook-Air.local>
2025-06-30 07:04:20 +0300

    Remove frontend submodule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,7 +10,7 @@ import { EventsModule } from './modules/events/events.module';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'db',
+      host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
